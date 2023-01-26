9d284711846d11a151e84bd774c2ca3f1e15d0c1
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-25 22:09:42 -0300

    Incluindo loop

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-
-int main() {
+int main()
+{
 
     printf("**************************************\n");
     printf("Bem vindo ao nosso jogo de adivinhação\n");
@@ -10,27 +10,38 @@ int main() {
     int numeroSecreto = 42;
 
     //%d -> espera um int
-    //printf("O número %d é secreto. Não conta pra ninguém\n", numeroSecreto);
+    // printf("O número %d é secreto. Não conta pra ninguém\n", numeroSecreto);
 
     int chute;
 
-    printf("Qual é o seu chute?\n");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d\n", chute);
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("Tentativa %d de 3\n", i);
+        printf("Qual é o seu chute?\n");
+        scanf("%d", &chute);
+        printf("Seu chute foi: %d\n", chute);
 
-    int acertou = (chute == numeroSecreto);
+        int acertou = (chute == numeroSecreto);
 
-    if(acertou){
-        printf("Parabéns! Você Acertou!!!\n");
-        printf("Jogue de novo, você é um bom jogador\n");
-    } else {
-        int chuteMaiorQueNumeroSecreto = (chute > numeroSecreto);
-        if(chuteMaiorQueNumeroSecreto) {
-            printf("Seu chute foi maior que o número secreto\n");
-        }else {
-            printf("Seu chute foi menor que o número secreto\n");
+        if (acertou)
+        {
+            printf("Parabéns! Você Acertou!!!\n");
+            printf("Jogue de novo, você é um bom jogador\n");
+
+            break;
+        }
+        else
+        {
+            int chuteMaiorQueNumeroSecreto = (chute > numeroSecreto);
+            if (chuteMaiorQueNumeroSecreto)
+            {
+                printf("Seu chute foi maior que o número secreto\n");
+            }
+            else
+            {
+                printf("Seu chute foi menor que o número secreto\n");
+            }
         }
     }
-
-
+    printf("Fim de Jogo!\n");
 }
